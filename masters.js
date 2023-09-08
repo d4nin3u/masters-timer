f56271a7aed973e7cf8e6ba2c841ca35c1a5dbe5
FreakyByte <michi.reitmeir@gmail.com>
2023-09-08 15:49:38 +0200

    Steadshot's skeleton

---
+++ masters.js
@@ -0,0 +1,16 @@
+//use strict;
+
+
+let player_times = {
+	"jago": "9:30:00",
+	"qlex": "9:45:00",
+	"freakybyte": "11:04:00"
+};
+
+function getTimeFromString(str) {
+	let bla = new Date(str);
+}
+
+console.log("test");
+console.log(getTimeFromString(player_times["jago"]));
+
